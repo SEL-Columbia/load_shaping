502e0cb6d9caa14e30884d88196f7932319b8a97
Daniel Soto <danielrsoto@gmail.com>
2012-04-19 19:55:37 -0400

    adds Panel class to deal with panel size and tilt

--- py/pvsim.py
+++ py/pvsim.py
@@ -1,6 +1,6 @@
 import scipy as sp
 import scipy.interpolate as spi
-from scipy import sin, cos, tan, arcsin, arccos
+from scipy import sin, cos, tan, arcsin, arccos, pi, exp
 
 
 
@@ -44,15 +44,17 @@ class Solar:
     lon = sp.radians(0)
     el_tilt = sp.radians(0)
     az_tilt = sp.radians(0)
-    A = 1000
+    solar_constant = 1.377
 
-    def __init__(self, A=1000, lat=0):
-        self.A = A
+    def __init__(self, lat=0):
         self.lat = sp.radians(lat)
+
+    def day_of_year(self, date):
+        return int(date.strftime('%j'))
 
     def declination(self, date):
         # '%j' gives day of year
-        day_of_year = int(date.strftime('%j'))
+        day_of_year = self.day_of_year(date)
         return sp.radians(23.45 * sp.sin(2 * sp.pi * (day_of_year - 81) / 365.0))
 
     def hour_angle(self, date):
@@ -75,19 +77,53 @@ class Solar:
         else:
             return (sp.pi - az)
 
+    def extraterrestrial_insolation(self, date):
+        day_of_year = self.day_of_year(date)
+        return self.solar_constant * (1 + 0.034 * cos(2 * pi * day_of_year / 365))
+
+    def apparent_extraterrestrial_flux(self, date):
+        day_of_year = self.day_of_year(date)
+        return 1160 + 75 * sin(2 * pi * (day_of_year - 275) / 365)
+
+    def optical_depth(self, date):
+        day_of_year = self.day_of_year(date)
+        return 0.174 + 0.035 * sin(2 * pi * (day_of_year - 100) / 365)
+
+    def air_mass_ratio(self, date):
+        return 1 / sin(self.elevation(date))
+
+    def direct_beam_radiation(self, date):
+        return self.apparent_extraterrestrial_flux(date) * exp(- self.optical_depth(date)
+                                                               * self.air_mass_ratio(date))
+
+
+'''
+'''
+class Panel:
+
+    def __init__(self, solar, area=1, efficiency = 0.20, el_tilt=0, az_tilt=0):
+        self.area = area
+        self.efficiency = efficiency
+        self.el_tilt = sp.radians(el_tilt)
+        self.az_tilt = sp.radians(az_tilt)
+        self.solar = solar
+
     def incidence_angle(self, date):
-        dec = self.declination(date)
-        el = self.elevation(date)
-        az = self.azimuth(date)
+        dec = self.solar.declination(date)
+        el = self.solar.elevation(date)
+        az = self.solar.azimuth(date)
         return arccos(cos(el) * cos(az - self.az_tilt) * sin(self.el_tilt)
                       + sin(el) * cos(self.el_tilt))
 
-    def insolation(self, date):
+    def radiation_normal_panel(self, date):
         # todo: no air mass correction
         # optical depth
         # attenuation
-        ins = self.A * cos(self.incidence_angle(date))
+        ins = self.solar.direct_beam_radiation(date) * cos(self.incidence_angle(date))
         if ins > 0:
             return ins
         else:
             return 0
+
+    def power(self, date):
+        return self.area * self.efficiency * self.radiation_normal_panel(date)
