22306d14bcf160eed4388d60c70af550a7da9aea
unknown <Mitchell@Mitchell-THINK.(none)>
2012-06-07 11:29:48 -0400

    Now finds discontinuities in the data sets. Counts the number of breaks and provides their indices
    Also subtracts off the parasitic load. If the minimum load is less than 40,
    then the minimum load is subtractred off. If the minimum load is greater
    than 40, the 40 is subtracted off.

--- Meter Data/ug data/HourlyDemandFrom5MinuteData.m
+++ Meter Data/ug data/HourlyDemandFrom5MinuteData.m
@@ -10,8 +10,21 @@ UG0800hr = UG0800(ixs,[1,2,4]);
 dateshr = datevec(UG0800hr(:,2));
 [r2,c2] = size(dateshr);
 k=0;
+breaks = [];
 for ix = 2:r2
-    if (dateshr(ix,4)~= (dateshr(ix-1,4)+1)) && ((dateshr(ix,4)~=0 && dateshr(ix-1,4)==23)&&(dateshr(ix,4)==0 && dateshr(ix-1,4)~=23))
+    if ((dateshr(ix,4)~=0 && dateshr(ix-1,4)==23)||(dateshr(ix,4)==0 && dateshr(ix-1,4)~=23))
         k = k+1;
+        breaks(k) = ix-1;
+    elseif (dateshr(ix,4)~= (dateshr(ix-1,4)+1)) 
+        if dateshr(ix-1,4)~=23
+            %dateshr(ix-1,:);
+            k= k +1;
+            breaks(k) = ix-1;
+        end
     end
+end
+if min(UG0800hr(:,3))<=40
+   UG0800hr(:,3) = UG0800hr(:,3) - min(UG0800hr(:,3));
+else
+   UG0800hr(:,3) = UG0800hr(:,3) - 40;
 end
