4d59d42fac5b3609d0e0e29c2ec6c2e8d824ec32
Mitch <mitchelllee99@gmail.com>
2012-06-22 14:31:57 -0400

    added files for Battery and PV Optimization

--- matlab/LEGP_Optimization/pvBatOpt.m
+++ matlab/LEGP_Optimization/pvBatOpt.m
@@ -1,8 +1,8 @@
 % Mitchell Lee
 % Shared Solar
 % Find mimimum cost battery/PV soluation for a Specified LEGP
-LEGPDesired = linspace(.01,.20,40);
-best = zeros(length(LEGPDesired),4);
+LEGPDesired = linspace(.01,.2,40);
+best = zeros(length(LEGPDesired),6);
 % call upon SuppDem Sum
 
 for jx = 1:length(LEGPDesired);
@@ -10,12 +10,13 @@ for jx = 1:length(LEGPDesired);
     pvStep = 100; % fineness by which PV size can be changed (Watts)
     batStep = 100; % finess by which Batter size may be changed (W-hr)
     pvCost = 0.1762;% Annual Payment for pv ($/Watt-yr)
-    batCost = 0.4021;% Annual Payment for battery capacity ($/W-hr-yr)
-    dates = MaliNTSData2005(:,1:4);
-    resource = MaliNTSData2005(:,5);
-    demand = lightDemandYear;
+    batCost = 0.0804;% Annual Payment for battery capacity ($/W-hr-yr)
+    dates = LuxorNTSData2005(:,1:4);
+    resource = LuxorNTSData2005(:,5);
+    demand = fridgeDemandYear;
     batMin = 0;
-    batCap = 100*max(lightDemandYear);
+    batCap = 100*max(fridgeDemandYear);
+    batPerDis = .50;
     pvCap = 20000;
     LEGP = 0;
     while LEGP <= LEGPDesired(jx)
@@ -31,13 +32,17 @@ for jx = 1:length(LEGPDesired);
         pvBatCurve(ix,:) = [batCap, pvCap];
         pvCap = pvCap +100;
     end
-    
+    %Correct for only allowable discharge of battery
+    pvBatCurve(:,1) = pvBatCurve(:,1)/batPerDis; 
     % Yearly Payment Cost
-    cost = pvBatCurve(:,1)*batCost+pvBatCurve(:,2)*pvCost;
-    [minCost, minRow] = min(cost);
+    totCost = pvBatCurve(:,1)*batCost+pvBatCurve(:,2)*pvCost;
+    [minCost, minRow] = min(totCost);
+    batMinCost = pvBatCurve(minRow,1)*batCost; 
+    pvMinCost = pvBatCurve(minRow,2)*pvCost;
+    
     % Cost Per kWh based on Yearly Payment Cost
-%    kWh_supp = sum(demand)*(1-LEGP_ach)/1000;        
-%    cost_kW = (pvBatCurve(:,1)*batCost+pvBatCurve(:,2)*pvCost)/kWh_supp;
-%    [minCost, minRow] = min(cost_kW);
-    best(jx,:) = [LEGPDesired(jx),minCost,pvBatCurve(minRow,:)];
+  kWh_supp = sum(demand)*(1-LEGP_ach)/1000;        
+  cost_kW = (pvBatCurve(:,1)*batCost+pvBatCurve(:,2)*pvCost)/kWh_supp;
+  [minCost, minRow] = min(cost_kW);
+  best(jx,:) = [LEGPDesired(jx),minCost,batMinCost,pvMinCost,pvBatCurve(minRow,:)];
 end
