1baf484b2233571082787dc6eaee883f3b22a771
Mitch <mitchelllee99@gmail.com>
2012-07-16 23:32:47 -0400

    turned makeAveDay.m into a function which only requires one week of demand (1x168 vector) to be specified. The output is an average daily demand profile.

--- matlab/functions/makeAveDay.m
+++ matlab/functions/makeAveDay.m
@@ -1,11 +1,17 @@
+function [ aveDays] = makeAveDay(demVec)
+%makeAveDay
 % Mitchell Lee
 % Shared Solar
 % Create an average hourly demand profile for a single day
 % Began on July 5, 2012
 
 aveDays = zeros(24,1);
+
 for ix = 1:24
-    aveDays(ix) = mean(fridgeDemandWeek(ix:24:144+ix));
+    aveDays(ix) = mean(demVec(ix:24:144+ix));
 end
 
+
+
+end
 
