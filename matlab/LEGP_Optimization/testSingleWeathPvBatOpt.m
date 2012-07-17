e0a0e17691c3255a26e9554f874730aa030729c7
Mitch <mitchelllee99@gmail.com>
2012-07-17 17:28:00 -0400

    Same functionaliyt as testMultiWeathPvBatOpt.m except that the energy balance/optimization algoritm is only completed for a single weather profile. The cost per kWh vs LEGP generated also only has the results for a single weather profile.

---
+++ matlab/LEGP_Optimization/testSingleWeathPvBatOpt.m
@@ -0,0 +1,29 @@
+% Mitchell Lee
+% Shared Solar 
+% Call on pvBatOptf for four different weather profiles
+% Script Began on July 9, 2012
+
+dates = MaliNTS(:,1:4);
+dates = [dates,ones(8760,2)];
+weather = MaliNTS(:,5);
+[r,c] = size(weather);
+lats = 13.45;
+demVec = fridgeDemandYear;
+LEGPVec = 0.01:0.01:0.20;
+
+sigma = lats;
+phi_c = 0;
+rho = 0.2;
+I_C = resourceCalc (dates,sigma,phi_c,weather,lats,rho);
+bestSingleLoc = pvBatOptf(dates,I_C,demVec,LEGPVec);
+
+%% Plot for ONE Cost vs LEGP Curve
+plot(bestSingleLoc(:,1),bestSingleLoc(:,2),'LineWidth',2)
+
+xlabel('LEGP','FontSize',18)
+ylabel('Cost Per kWhr (USD)','FontSize',18)
+legend('Mali')
+set(gca,'FontSize',12)
+axis tight
+hold off
+
